8f8eac57462b116d2c678bde3fb3916d78b294a1
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-01 16:48:28 +0400

    feat: add number schema

---
+++ src/Schema/NumberSchema.ts
@@ -0,0 +1,8 @@
+import { Schema } from './Schema';
+import type { SchemaOptions } from './SchemaOptions';
+
+export class NumberSchema<TInput = any> extends Schema<TInput, number> {
+  constructor(options: SchemaOptions<TInput> = {}) {
+    super({ ...options, typeConstructor: Number, type: 'number' });
+  }
+}
