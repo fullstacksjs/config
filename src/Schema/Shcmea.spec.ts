fb3a6ac11c9013987a7d343c4f003b114d426d02
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-01 16:42:27 +0400

    test: move schema tests to Schema.spec.ts

---
+++ src/Schema/Shcmea.spec.ts
@@ -0,0 +1,39 @@
+import { GuardError } from '../Guard';
+import { Schema } from './Schema';
+
+describe('Schema', () => {
+  it('should throw if a required config is undefined', () => {
+    const schema = new Schema({
+      type: 'string',
+      typeConstructor: String,
+    }).require();
+    schema.key = 'port';
+
+    expect(() => schema.validate()).toThrow(
+      new GuardError(
+        `Invalid configuration: The "port" is required but the given value is "undefined"`,
+      ),
+    );
+  });
+
+  it('should use default value when value is undefined', () => {
+    const schema = new Schema({
+      type: 'string',
+      typeConstructor: String,
+      default: '3000',
+    }).setValue();
+
+    expect(schema.parse()).toBe('3000');
+  });
+
+  it('should use type constructor for type coercion', () => {
+    const schema = new Schema<any, string>({
+      type: 'string',
+      typeConstructor: v => `parsed ${v}`,
+      default: '3000',
+      coerce: true,
+    }).setValue(3000);
+
+    expect(schema.parse()).toBe('parsed 3000');
+  });
+});
