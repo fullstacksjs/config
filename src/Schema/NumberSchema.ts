21499839b359c51c1d2e9541c66656f22f461ba2
Alireza Safaierad <frontendmonster@gmail.com>
2023-10-28 01:47:42 +0400

    fix: make default value type-safe

--- src/Schema/NumberSchema.ts
+++ src/Schema/NumberSchema.ts
@@ -1,9 +1,31 @@
-import { MaxNumberGuard, MinNumberGuard } from '../Guard';
+import type { Guard } from '../Guard';
 import { Schema } from './Schema';
 import type { SchemaOptions } from './SchemaOptions';
 
+class MinNumberGuard implements Guard<number> {
+  constructor(private min: number) {}
+
+  validate(input: number, field: number) {
+    if (input < this.min)
+      throw new RangeError(
+        `Invalid configuration: The "${field}" expected to be more than or equal to "${this.min}" but "${input}" was provided`,
+      );
+  }
+}
+
+class MaxNumberGuard implements Guard<number> {
+  constructor(private max: number) {}
+
+  validate(input: number, field: number) {
+    if (input > this.max)
+      throw new RangeError(
+        `Invalid configuration: The "${field}" expected to be less than or equal to "${this.max}" but "${input}" was provided`,
+      );
+  }
+}
+
 export class NumberSchema<TInput = any> extends Schema<TInput, number> {
-  constructor(options: SchemaOptions<TInput> = {}) {
+  constructor(options: SchemaOptions<number> = {}) {
     super({ ...options, typeConstructor: Number, type: 'number' });
   }
 
--- src/Schema/StringSchema.ts
+++ src/Schema/StringSchema.ts
@@ -2,7 +2,7 @@ import { Schema } from './Schema';
 import type { SchemaOptions } from './SchemaOptions';
 
 export class StringSchema<TInput = any> extends Schema<TInput, string> {
-  constructor(options: SchemaOptions<TInput> = {}) {
+  constructor(options: SchemaOptions<string> = {}) {
     super({ ...options, typeConstructor: String, type: 'string' });
   }
 }
