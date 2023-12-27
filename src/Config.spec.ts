480b9a1f14156d46df89bc68c33e648459b3de78
ASafaierad <frontendmonster@gmail.com>
2023-12-27 21:30:05 +0100

    feat: add path getter to get method

--- src/Config.spec.ts
+++ src/Config.spec.ts
@@ -6,35 +6,41 @@ describe('Config', () => {
     const config = new Config({
       s: Config.string(),
       n: Config.number().required(),
-      foo: Config.object({
+      nested: Config.object({
         foo1: Config.string().required(),
         foo2: Config.object({ foo3: Config.boolean() }),
       }),
       arr: Config.array(Config.string()),
-    })
-      .parse({
-        s: 's',
-        n: 0,
-        foo: { foo1: 'foo1', foo2: { foo3: false } },
-        arr: ['a', 'b'],
-      })
-      .getAll();
-
-    expect(config).toEqual({
+    }).parse({
       s: 's',
       n: 0,
-      foo: { foo1: 'foo1', foo2: { foo3: false } },
+      nested: { foo1: 'foo1', foo2: { foo3: false } },
       arr: ['a', 'b'],
     });
+
+    const configs = config.getAll();
+    const nested = config.get('nested');
+    const foo2 = config.get('nested.foo2');
+    const foo3 = config.get('nested.foo2.foo3');
+
+    expect(configs).toEqual({
+      s: 's',
+      n: 0,
+      nested: { foo1: 'foo1', foo2: { foo3: false } },
+      arr: ['a', 'b'],
+    });
+    expect(nested).toEqual({ foo1: 'foo1', foo2: { foo3: false } });
+    expect(foo2).toEqual({ foo3: false });
+    expect(foo3).toBe(false);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     type Test = Expect<
       Equals<
-        typeof config,
+        typeof configs,
         {
           s: string | undefined;
           n: number;
-          foo: { foo1: string; foo2: { foo3: boolean | undefined } };
+          nested: { foo1: string; foo2: { foo3: boolean | undefined } };
           arr: string[];
         }
       >
