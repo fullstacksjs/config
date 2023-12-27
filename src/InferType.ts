445c0ebad555597096c298da529dbfc6a653942b
ASafaierad <frontendmonster@gmail.com>
2023-12-27 01:50:46 +0100

    fix: fix object schema type inference

--- src/InferType.ts
+++ src/InferType.ts
@@ -1,7 +1,13 @@
 import type { Schema } from './Schema';
 
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {}; // eslint-disable-line @typescript-eslint/ban-types
+
 export type InferType<T extends Record<string, Schema>> = {
-  [K in keyof T]: T[K]['isRequired'] extends true
+  [K in keyof T]: T[K] extends Schema<object>
+    ? Prettify<InferType<T[K]['value']>>
+    : T[K]['isRequired'] extends true
     ? NonNullable<T[K]['value']>
     : T[K]['value'];
 };
