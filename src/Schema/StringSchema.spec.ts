c07e2ad269fd5d92eaa560947d96730364b8be58
ASafaierad <frontendmonster@gmail.com>
2023-12-28 19:31:23 +0100

    feat: add string min and max length

--- src/Schema/StringSchema.spec.ts
+++ src/Schema/StringSchema.spec.ts
@@ -17,4 +17,66 @@ describe('String Schema', () => {
 
     expect(schema.parse()).toBe('3000');
   });
+
+  it('should throw when string length is less than min', () => {
+    const schema = new StringSchema({ coerce: false })
+      .min(3)
+      .setValue(' '.repeat(1));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).toThrow(
+      new TypeError(
+        'Invalid configuration: The "key" length expected to be more than or equal to "3" but "1" was provided',
+      ),
+    );
+  });
+
+  it('should not throw when string length is more than min', () => {
+    const schema = new StringSchema({ coerce: false })
+      .min(3)
+      .setValue(' '.repeat(4));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).not.toThrow();
+  });
+
+  it('should not throw when string length is equal than min', () => {
+    const schema = new StringSchema({ coerce: false })
+      .min(3)
+      .setValue(' '.repeat(3));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).not.toThrow();
+  });
+
+  it('should throw when string length is more than max', () => {
+    const schema = new StringSchema({ coerce: false })
+      .max(1)
+      .setValue(' '.repeat(3));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).toThrow(
+      new TypeError(
+        'Invalid configuration: The "key" length expected to be less than or equal to "1" but "3" was provided',
+      ),
+    );
+  });
+
+  it('should not throw when string length is less than max', () => {
+    const schema = new StringSchema({ coerce: false })
+      .max(4)
+      .setValue(' '.repeat(3));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).not.toThrow();
+  });
+
+  it('should not throw when string length is equal than max', () => {
+    const schema = new StringSchema({ coerce: false })
+      .max(3)
+      .setValue(' '.repeat(3));
+    schema.key = 'key';
+
+    expect(() => schema.validate()).not.toThrow();
+  });
 });
