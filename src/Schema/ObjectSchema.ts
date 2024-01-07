7f7e5bdbb88bfbec72f5cd1f5e7c7312176b5de5
ASafaierad <frontendmonster@gmail.com>
2024-01-07 19:18:01 +0100

    fix: fix parsing nested object

--- src/Schema/ObjectSchema.ts
+++ src/Schema/ObjectSchema.ts
@@ -17,19 +17,38 @@ class ObjectGuard implements Guard<Record<string, Schema>> {
   }
 }
 
-export class ObjectSchema<TInput = any> extends Schema<
-  TInput,
-  TInput,
-  boolean
-> {
+export class ObjectSchema<
+  TInput extends Record<string, unknown> = any,
+> extends Schema<TInput, TInput, boolean> {
   protected type = 'object';
 
-  constructor(schema: Record<string, Schema<any, any, boolean>>) {
+  constructor(private schema: Record<string, Schema<any, any, boolean>>) {
     super({
       typeConstructor: x => x,
       initialGuards: [new TypeGuard('object')],
     });
     this.required();
     this.guards.push(new ObjectGuard(schema));
+  }
+
+  public override setValue(input?: TInput) {
+    this.input = input;
+
+    if (typeof input !== 'object') {
+      this.value = input;
+    } else {
+      this.value = Object.keys(input).reduce((acc, key) => {
+        const schema = this.schema[key];
+
+        if (!schema) return acc;
+
+        schema.setValue(input[key]);
+        // @ts-expect-error It's hard
+        acc[key] = schema.parse();
+        return acc;
+      }, {}) as TInput;
+    }
+
+    return this;
   }
 }
